8f511a07d184c872d5e5622b615bc7ff908f90a6
DmitryGavrilovSkypro <ngmademon@gmail.com>
2023-03-12 20:09:01 +0300

    hw-2

---
+++ hw-2.js
@@ -0,0 +1,64 @@
+// example1//
+let a = Number(prompt('10'))
+alert (a)
+
+// example2//
+const releaseiPhone = 2007
+alert (releaseiPhone)
+
+// example3//
+const creatorJS = 'Брендан Эйх'
+alert (creatorJS)
+
+// example4//
+let number10 = 10
+let number2 = 2
+let sum = number10 + number2
+let difference = number10 - number2
+let multiplication = number10 * number2
+let division = number10 / number2
+alert (sum)
+alert (difference)
+alert (multiplication)
+alert (division)
+
+// example5//
+let number52 = 2
+let result = (number52 ** 5)
+alert (result)
+
+// example6//
+let number9 = 9
+let number62 = 2
+let remainder = (number9 % number62)
+alert (remainder) 
+
+// example7//
+let num = 1;
+num = num + 5;
+num = num - 3;
+num = num * 7;
+num = num / 3;
+num = num + 1;
+num = num - 1;
+alert(num);
+
+// example8//
+let age = prompt ('Сколько вам лет')
+alert(age)
+// example9//
+
+const user = {
+    name : prompt('Dmitry'),
+    age : 32,
+    isAdmin : true,
+}
+user.city = 'city of residence' 
+delete city;
+let info = prompt ('Какую информацию хотите узнать о пользователе?')
+alert (info)
+
+
+// example10//
+let userName = prompt('Введите ваше имя');
+alert(`Привет, ${userName}!`);
